7e0d47e00cc31875429ae4ec23c1c18b2f17d291
Moses Lee <leecmoses@gmail.com>
2022-04-26 16:53:51 -0400

    Implement smooth scrolling

--- script.js
+++ script.js
@@ -29,3 +29,28 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", (e) => {
+  const s1coords = section1.getBoundingClientRect();
+
+  // Scrolling (old way)
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+  // console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+  // console.log(
+  //   "height/width viewport",
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  // Modern Way of Scrolling
+  section1.scrollIntoView({ behavior: "smooth" });
+});
