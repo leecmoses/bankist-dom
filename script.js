342602554f6d3f1eae9af0678d5ad6a4594029c1
Moses Lee <leecmoses@gmail.com>
2022-04-27 15:05:29 -0400

    Implement smooth scrolling using even delegation

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,8 +32,8 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
+///////////////////////////////////////
+// Button scrolling
 
 btnScrollTo.addEventListener("click", (e) => {
   const s1coords = section1.getBoundingClientRect();
@@ -53,4 +55,29 @@ btnScrollTo.addEventListener("click", (e) => {
 
   // Modern Way of Scrolling
   section1.scrollIntoView({ behavior: "smooth" });
+});
+
+///////////////////////////////////////
+// Page navigation
+
+// document.querySelectorAll(".nav__link").forEach((e) => {
+//   e.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// Event Delegation - adding one event listener to the parent instead of all the children
+// 1. Add event listener to common parent element
+// 2. Determine what element orignated the event
+
+document.querySelector(".nav__links").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
